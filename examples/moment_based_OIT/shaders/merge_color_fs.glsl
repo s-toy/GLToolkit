f65c63cbd9f8047b02f067d0bc62d8a17ea21ed1
xfsong <songxiaofeng96@gmail.com>
2020-04-24 17:19:20 +0800

    update

---
+++ examples/moment_based_OIT/shaders/merge_color_fs.glsl
@@ -0,0 +1,17 @@
+#version 430 core
+
+uniform sampler2D uOpaqueColorTex;
+uniform sampler2D uAccumulatedReflectionTex;
+uniform sampler2D uAccumulatedTransmissionTex;
+
+layout(location = 0) in vec2 _inTexCoord;
+
+void main()
+{
+	vec3 bgColor = texture(uOpaqueColorTex, _inTexCoord).rgb;
+	vec4 A = texture(uAccumulatedReflectionTex, _inTexCoord).rgba;
+	vec3 B = texture(uAccumulatedTransmissionTex, _inTexCoord).rgb;
+	
+	vec3 color = bgColor * B + (1.0 - B) * A.rgb / A.a;
+	gl_FragColor = vec4(color, 1.0);
+}
